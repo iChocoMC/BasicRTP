1f773258b2f6f8c72e5e1c6e8d1cb6a3fe6b9322
iChocoMC_ <ichocomc@gmail.com>
2022-11-13 16:45:53 -0300

    minor optimization

--- src/main/java/ichoco/basicrtp/utils/MethodUtil.java
+++ src/main/java/ichoco/basicrtp/utils/MethodUtil.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 import ichoco.basicrtp.BasicRtpMain;
@@ -14,15 +15,16 @@ public abstract class MethodUtil{
     private int maxRadius = BasicRtpMain.getConfiguration().getInt("max-radius");;
 
     public void execute(Player player, int x, int z){
-        Location newLoc;
+        Block block;
         World world = player.getWorld();
 		world.loadChunk(x, z);
 
         for(short i = 5; i < 255; i++){
-            newLoc = new Location(world, x, i , z);
-            if (newLoc.getBlock().isEmpty()){
-                player.teleport(newLoc);
-                MessageUtil.sendMessage(player, newLoc);
+            block = world.getBlockAt(x, i, z);
+            if (block.isEmpty()){
+                Location loc = block.getLocation();
+                player.teleport(loc);
+                MessageUtil.sendMessage(player, loc);
                 break;
             }
         }
